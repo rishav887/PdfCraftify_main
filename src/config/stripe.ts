d9f8a449b8d479b056f6cc6e578cb8124a1edbe4
Rishav Trivedi <rishabtrivedi87@gmail.com>
2023-10-12 21:52:12 +0530

    Update stripe.ts
    cost api updated

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -20,7 +20,7 @@ export const PLANS = [
     price: {
       amount: 7,
       priceIds: {
-        test: 'price_1NzKYkSDFWaOpArYRSAsBInq',
+        test: 'price_1O0RdcSDFWaOpArYmvxBCI3A',
         production: 'price_1O0RKQSDFWaOpArY6vZ6ysuK',
       },
     },
