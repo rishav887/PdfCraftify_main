014d770a2a747cc6ad9813cc95f80e0994e821ab
Rishav Trivedi <rishabtrivedi87@gmail.com>
2023-10-28 09:57:30 +0530

    commnted userid temp

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -184,12 +184,12 @@ export const appRouter = router({
   getFile: privateProcedure
     .input(z.object({ key: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      const { userId } = ctx
+     // const { userId } = ctx
 
       const file = await db.file.findFirst({
         where: {
           key: input.key,
-          userId,
+         // userId,
         },
       })
 
